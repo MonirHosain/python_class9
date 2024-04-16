031cdab2af24c459bde405f085126659369e3b92
unknown <me.monirhosain@gmail.com>
2024-04-16 08:50:11 +0600

    F

---
+++ main.py
@@ -0,0 +1,29 @@
+# হ্যাস চিহ্ন ব্যাবহার করা হয় কোন কিছু কমেন্ট করতে 
+# print() => কোন কিছু প্রিন্ট করতে print function ব্যাবহার করা হয় 
+# type() => কোন কিছুর টাইপ বের করতে ফাংশনটি ব্যাবহার করা হয়
+# input() => User এর কাছ থেকে কোন ডাটা আনার জন্য ব্যাবহার করা হয়
+# variable = কোন কিছুর ভ্যালু জমা রাখতে ব্যাবহার করা হয়
+
+
+
+
+
+print(27)
+
+
+
+
+
+print("Hello")
+
+
+
+print(type(27))
+# print(type("Hello"))
+
+person = "Habib"
+
+first_name = input("Whats your first name: ")
+last_name = input("Whats your last name: ")
+
+print("Thenge thenge to " + first_name + " " + last_name)
