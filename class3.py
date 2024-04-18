3ea87515d52e6ee18a907e42959047ad52cdb8e3
Monir Hosain <me.monirhosain@gmail.com>
2024-04-18 08:53:29 +0600

    class 3 added

---
+++ class3.py
@@ -0,0 +1,29 @@
+# my_text = "Hello world"
+# my_num = 23 
+# my_num_2 = 23 
+
+# print(my_text)
+# print(type(my_text))
+
+# print(my_num)
+# print(type(my_num))
+
+# print(my_num_2)
+
+
+
+
+input_num = int(input("Type a sample number : "))
+input_num_2 = int(input("Type another sample number : "))
+
+
+print(input_num)
+print(input_num_2)
+ 
+sum_num = input_num + input_num_2
+
+print(str(input_num) + " + " + str(input_num_2) + " = " + str(sum_num)  )
+
+
+
+
